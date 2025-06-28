835588a8aba6dda49713c66bcc3fe4acc8b5159d
ProstoMakcc <antimaks1974@gmail.com>
2025-06-28 15:56:21 +0200

    Upgraded messenger

--- core/views.py
+++ core/views.py
@@ -4,4 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def homepage(request):
-    return render(request, 'homepage.html')
+    context = {
+        'chats': request.user.chats.all(),
+    }
+    return render(request, 'homepage.html', context)
--- messenger/views.py
+++ messenger/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Chat
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
-
-@login_required
 def chat(request, pk):
-    chat = get_object_or_404(Chat, pk=pk)
+    chat = get_object_or_404(Chat, id=pk)
     
-    messages = chat.messages.all()
+    messages = chat.messages.all().order_by('timestamp')
 
-    return render(request, 'messenger/chat.html', {'chat': chat, 'messages': messages})
+    return JsonResponse({'messages': list(messages.values())})
